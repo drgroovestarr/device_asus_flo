e2f984c581518d2a955f53e3cc31790475d6f3b9
oraixearl <smith.jesse79@gmail.com>
2017-02-11 12:18:05 -0700

    Broke-it

---
+++ broken.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/broken/config/common.mk)
+
+PRODUCT_NAME := broken_flo
+PRODUCT_DEVICE := flo
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Nexus 7 2013
+PRODUCT_MANUFACTURER := ASUS
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/asus/flo/device.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=razor \
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MOB30X/3036618:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MOB30X 3036618 release-keys"
+    
